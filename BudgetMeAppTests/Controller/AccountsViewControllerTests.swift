9ae26565cf0860c2ebca441e67dd64433d330f07
Christian Ray Leovido <christian.leovido@gmail.com>
2020-03-29 19:42:16 +0100

    Create AccountsViewControllerTests.swift

---
+++ BudgetMeAppTests/Controller/AccountsViewControllerTests.swift
@@ -0,0 +1,46 @@
+//
+//  AccountsViewControllerTests.swift
+//  BudgetMeAppTests
+//
+//  Created by Christian Leovido on 29/03/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+import XCTest
+@testable import BudgetMeApp
+
+class AccountsViewControllerTests: XCTestCase {
+
+    var viewController: AccountsViewController!
+
+    override func setUp() {
+        viewController = sutNavigationSetup()
+    }
+
+    override func tearDown() {
+        viewController = nil
+    }
+
+    func testLifeCycle() {
+        viewController.viewWillAppear(true)
+        viewController.viewDidLoad()
+        viewController.viewDidAppear(true)
+        viewController.viewWillAppear(true)
+    }
+
+    func sutNavigationSetup<T>() -> T {
+
+        viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "AccountsViewController") as? AccountsViewController
+
+        let navigationController = UINavigationController()
+
+        UIApplication.shared.keyWindow!.rootViewController = navigationController
+        navigationController.pushViewController(viewController, animated: false)
+
+        viewController = navigationController.topViewController as? AccountsViewController
+        viewController.loadView()
+
+        return viewController as! T
+    }
+
+}
