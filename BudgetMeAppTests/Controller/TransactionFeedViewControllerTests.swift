10e60d738fd6f9c50b22dc7dda4decd7d59573b0
Christian Ray Leovido <christian.leovido@gmail.com>
2020-03-29 19:42:19 +0100

    Create TransactionFeedViewControllerTests.swift

---
+++ BudgetMeAppTests/Controller/TransactionFeedViewControllerTests.swift
@@ -0,0 +1,56 @@
+//
+//  TransactionFeedViewControllerTests.swift
+//  BudgetMeAppTests
+//
+//  Created by Christian Leovido on 29/03/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+import XCTest
+
+@testable import BudgetMeApp
+
+class TransactionFeedViewControllerTests: XCTestCase {
+
+    var viewController: TransactionFeedViewController!
+
+    override func setUp() {
+
+        viewController = sutNavigationSetup()
+
+    }
+
+    override func tearDown() {
+        viewController = nil
+    }
+
+    func testExample() {
+        viewController.viewWillAppear(true)
+        viewController.viewDidLoad()
+        viewController.viewDidAppear(true)
+        viewController.viewWillAppear(true)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    func sutNavigationSetup<T>() -> T {
+
+        viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TransactionFeedViewController") as? TransactionFeedViewController
+
+        let navigationController = UINavigationController()
+
+        UIApplication.shared.keyWindow!.rootViewController = navigationController
+        navigationController.pushViewController(viewController, animated: false)
+
+        viewController = navigationController.topViewController as? TransactionFeedViewController
+        viewController.loadView()
+
+        return viewController as! T
+    }
+
+}
