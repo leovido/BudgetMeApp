15cca2a2275e44238685b6c5a0d68e5b3aa56e11
Christian Ray Leovido <christian.leovido@gmail.com>
2020-03-29 18:45:30 +0100

    Create TransactionDetailsViewController.swift

---
+++ BudgetMeApp/Controller/TransactionDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TransactionDetailsViewController.swift
+//  BudgetMeApp
+//
+//  Created by Christian Leovido on 29/03/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+import UIKit
+
+class TransactionDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
