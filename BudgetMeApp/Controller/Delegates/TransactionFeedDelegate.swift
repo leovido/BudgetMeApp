95aa179d19afc7633ddeba81fde4d37434b753d1
Christian Ray Leovido <christian.leovido@gmail.com>
2020-03-29 21:46:52 +0100

    Create TransactionFeedDelegate.swift

---
+++ BudgetMeApp/Controller/Delegates/TransactionFeedDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  TransactionFeedDelegate.swift
+//  BudgetMeApp
+//
+//  Created by Christian Leovido on 29/03/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension TransactionFeedViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+
+        guard let header: UITableViewHeaderFooterView = view as? UITableViewHeaderFooterView else {
+            return
+        }
+
+        header.textLabel?.textColor = UIColor.black
+        header.textLabel?.font = UIFont(name: "Futura", size: 21)
+        header.textLabel?.textAlignment = .left
+        header.backgroundColor = .clear
+        header.backgroundView?.backgroundColor = .red
+        header.contentView.backgroundColor = .clear
+
+        let headerTitle = dataSource[section].header
+
+        header.textLabel?.text = headerTitle
+
+    }
+}
