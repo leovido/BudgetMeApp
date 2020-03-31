ff2dfc9fb393e421f8d68eb60c54ee25e584271a
Christian Ray Leovido <christian.leovido@gmail.com>
2020-03-31 14:52:56 +0100

    add downloadStatements for PDF and CSV [WIP]

--- BudgetMeApp/Networking/Accounts/STAccountService.swift
+++ BudgetMeApp/Networking/Accounts/STAccountService.swift
@@ -15,8 +15,10 @@ enum STAccountService {
     case getBalance(accountId: String)
     case getConfirmationOfFunds(accountId: String)
     case getAvailableStatementPeriods(accountId: String)
-    case downloadStatement(accountId: String)
-    case downloadStatementForDateRange(accountId: String, start: DateTime, end: DateTime)
+    case downloadStatementPDF(accountId: String, yearMonth: String)
+    case downloadStatementCSV(accountId: String, yearMonth: String)
+    case downloadStatementPDFForDateRange(accountId: String, start: DateTime, end: DateTime)
+    case downloadStatementCSVForDateRange(accountId: String, start: DateTime, end: DateTime)
 }
 
 extension STAccountService: AuthorizedTargetType {
@@ -43,9 +45,11 @@ extension STAccountService: TargetType {
             return "/accounts/\(accountId)/confirmation-of-funds"
         case .getAvailableStatementPeriods(let accountId):
             return "/accounts/\(accountId)/available-periods"
-        case .downloadStatement(let accountId):
+        case .downloadStatementPDF(let accountId, _),
+             .downloadStatementCSV(let accountId, _):
             return "/accounts/\(accountId)/statement/download"
-        case .downloadStatementForDateRange(let accountId):
+        case .downloadStatementPDFForDateRange(let accountId),
+             .downloadStatementCSVForDateRange(let accountId):
             return "/accounts/\(accountId)/statement/downloadForDateRange"
         }
     }
@@ -59,13 +63,34 @@ extension STAccountService: TargetType {
     }
 
     public var task: Task {
-        return .requestPlain
+        switch self {
+
+        case .downloadStatementPDF(_, let yearMonth),
+             .downloadStatementCSV(_, let yearMonth):
+
+            return .downloadParameters(parameters: ["yearMonth": yearMonth],
+                                       encoding: URLEncoding.default,
+                                       destination: DefaultDownloadDestination)
+
+        case .downloadStatementPDFForDateRange(_, let start, let end),
+             .downloadStatementCSVForDateRange(_, let start, let end):
+
+            return .downloadParameters(parameters: ["start": start, "end": end],
+                                       encoding: URLEncoding.default,
+                                       destination: DefaultDownloadDestination)
+
+        default:
+            return .requestPlain
+        }
     }
 
     public var headers: [String: String]? {
         switch self {
-        case .downloadStatement, .downloadStatementForDateRange:
+        case .downloadStatementPDF, .downloadStatementPDFForDateRange:
             return ["Accept": "application/pdf",
+                    "User-agent": "Christian Ray Leovido"]
+        case .downloadStatementCSV, .downloadStatementCSVForDateRange:
+            return ["Accept": "text/csv",
                     "User-agent": "Christian Ray Leovido"]
         default:
             return ["Accept": "application/json",
@@ -77,4 +102,12 @@ extension STAccountService: TargetType {
     public var validationType: ValidationType {
         return .successAndRedirectCodes
     }
+}
+
+private let DefaultDownloadDestination: DownloadDestination = { temporaryURL, response in
+
+    let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    let fileURL = documentsURL.appendingPathComponent(response.suggestedFilename!)
+    return (fileURL, [.removePreviousFile, .createIntermediateDirectories])
+
 }
