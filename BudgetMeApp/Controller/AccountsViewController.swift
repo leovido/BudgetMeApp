54934af42b46d45f13d4e3848a9e89d73fc28768
Christian Ray Leovido <christian.leovido@gmail.com>
2020-03-31 14:53:28 +0100

    setup download statement button

--- BudgetMeApp/Controller/AccountsViewController.swift
+++ BudgetMeApp/Controller/AccountsViewController.swift
@@ -23,6 +23,7 @@ extension UIViewController {
 class AccountsViewController: UIViewController {
 
     @IBOutlet weak var accountsTableView: UITableView!
+    @IBOutlet weak var downloadStatementButton: UIButton!
 
     let disposeBag: DisposeBag = DisposeBag()
     let viewModel: AccountsViewModel = AccountsViewModel()
@@ -39,10 +40,80 @@ class AccountsViewController: UIViewController {
 
         setupBinding()
         setupErrorBindings()
-
+        setupButtonDownload()
         navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationController?.title = "Welcome to BudgetMe"
 
+    }
+
+    func presentDownloadAlert() {
+
+        let alert = UIAlertController(title: "Select file type for statement", message: "", preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "PDF", style: .default, handler: { _ in
+
+            let yearMonth = alert.textFields![0].text!
+
+            self.viewModel.downloadPDFStatement(accountId: Session.shared.accountId, yearMonth: yearMonth)
+                .asSingle()
+                .subscribe { event in
+                    switch event {
+                        case .success:
+                            self.showSuccessAlert()
+                        case .error(let error):
+                            self.viewModel.errorPublisher.onNext(error)
+                    }
+            }
+            .disposed(by: self.disposeBag)
+
+        }))
+
+        alert.addAction(UIAlertAction(title: "CSV", style: .default, handler: { _ in
+
+            let yearMonth = alert.textFields![1].text!
+
+            self.viewModel.downloadCSVStatement(accountId: Session.shared.accountId, yearMonth: yearMonth)
+                .asSingle()
+                .subscribe { event in
+                    switch event {
+                        case .success:
+                            self.showSuccessAlert()
+                        case .error(let error):
+                            self.viewModel.errorPublisher.onNext(error)
+                    }
+            }
+            .disposed(by: self.disposeBag)
+
+        }))
+
+        alert.addTextField { tf in
+            tf.placeholder = "e.g. 2020-03 PDF"
+        }
+
+        alert.addTextField { tf in
+            tf.placeholder = "e.g. 2020-03 CSV"
+        }
+
+        self.present(alert, animated: true, completion: nil)
+
+    }
+
+    func showSuccessAlert() {
+
+        let alert = UIAlertController(title: "Download success", message: "", preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    func setupButtonDownload() {
+        downloadStatementButton.rx.tap
+        .asObservable()
+            .subscribe { _ in
+                self.presentDownloadAlert()
+            }
+        .disposed(by: disposeBag)
     }
 
     func setupErrorBindings() {
