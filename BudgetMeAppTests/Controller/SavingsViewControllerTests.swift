620a213152d212c646c87d7e755585cc5a9f6ef1
Christian Ray Leovido <christian.leovido@gmail.com>
2020-03-31 18:11:38 +0100

    add present alert tests

--- BudgetMeAppTests/Controller/SavingsViewControllerTests.swift
+++ BudgetMeAppTests/Controller/SavingsViewControllerTests.swift
@@ -28,13 +28,36 @@ class SavingsViewControllerTests: XCTestCase {
         viewController.viewWillAppear(true)
     }
 
+    func testAlert() {
+
+        let expectation = XCTestExpectation(description: "show alert")
+
+        viewController.presentAlert()
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
+
+            let alert = UIApplication.shared.windows.first!.rootViewController?.presentedViewController as! UIAlertController
+
+            let actionTitles = alert.actions.compactMap({ $0.title })
+
+            XCTAssert(alert.title == "Saving goal name")
+
+            XCTAssert(actionTitles[0] == "Yes")
+            XCTAssert(actionTitles[1] == "Cancel")
+
+            expectation.fulfill()
+        })
+        wait(for: [expectation], timeout: 1.5)
+
+    }
+
     func sutNavigationSetup<T>() -> T {
 
         viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SavingsViewController") as? SavingsViewController
 
         let navigationController = UINavigationController()
 
-        UIApplication.shared.keyWindow!.rootViewController = navigationController
+        UIApplication.shared.windows.first!.rootViewController = navigationController
         navigationController.pushViewController(viewController, animated: false)
 
         viewController = navigationController.topViewController as? SavingsViewController
