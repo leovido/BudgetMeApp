5f2cec47c2ac6f12153347fa6f61104f0dd8c889
Christian Ray Leovido <christian.leovido@gmail.com>
2020-04-01 23:33:49 +0100

    Create UIViewController+ActivityIndicator.swift

---
+++ BudgetMeApp/View/ActivityIndicator/UIViewController+ActivityIndicator.swift
@@ -0,0 +1,12 @@
+//
+//  UIViewController+ActivityIndicator.swift
+//  BudgetMeApp
+//
+//  Created by Christian Leovido on 01/04/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+import UIKit
+import NVActivityIndicatorView
+
+extension UIViewController: NVActivityIndicatorViewable {}
