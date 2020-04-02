c81600b46452f41d9ab320bcae94b0fe79c17879
Christian Ray Leovido <christian.leovido@gmail.com>
2020-04-02 16:12:55 +0100

    Create TransactionFeedViewModel+Extensions.swift

---
+++ BudgetMeAppTests/ViewModelTests/TransactionFeedViewModelTests/TransactionFeedViewModel+Extensions.swift
@@ -0,0 +1,56 @@
+//
+//  TransactionFeedViewModel+Extensions.swift
+//  BudgetMeAppTests
+//
+//  Created by Christian Leovido on 02/04/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+@testable import BudgetMeApp
+
+extension TransactionFeedViewModelTests {
+
+    enum STTransactionSuccessTestCases: String {
+        case browse
+        case dateRange
+    }
+
+    var bundle: Bundle {
+        return Bundle(for: type(of: self) as! AnyClass)
+    }
+
+    func makeMoyaSuccessStub<T: TargetType>(type: STTransactionSuccessTestCases) -> MoyaProvider<T> {
+
+        #if DEBUG
+        let url = bundle.url(forResource: "transactions_success_" + type.rawValue, withExtension: "json")!
+        let data = try! Data(contentsOf: url)
+
+        let serverEndpointSuccess = { (target: T) -> Endpoint in
+            return Endpoint(url: URL(target: target).absoluteString,
+                            sampleResponseClosure: { .networkResponse(200, data) },
+                            method: target.method,
+                            task: target.task,
+                            httpHeaderFields: target.headers)
+        }
+
+        let serverStubSuccess = MoyaProvider<T>(
+            endpointClosure: serverEndpointSuccess,
+            stubClosure: MoyaProvider.immediatelyStub,
+            plugins: [
+                AuthPlugin(tokenClosure: { return Session.shared.token })
+            ]
+        )
+
+        return serverStubSuccess
+
+        #elseif STAGING
+        return MoyaNetworkManagerFactory.makeManager()
+        #endif
+
+    }
+
+}
+
