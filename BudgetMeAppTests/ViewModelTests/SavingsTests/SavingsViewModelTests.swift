64b974e5c5762865b67276efb20da9cd3143a15f
Christian Ray Leovido <christian.leovido@gmail.com>
2020-04-02 16:12:57 +0100

    Create SavingsViewModelTests.swift

---
+++ BudgetMeAppTests/ViewModelTests/SavingsTests/SavingsViewModelTests.swift
@@ -0,0 +1,37 @@
+//
+//  SavingsViewModelTests.swift
+//  BudgetMeAppTests
+//
+//  Created by Christian Leovido on 02/04/2020.
+//  Copyright © 2020 Christian Leovido. All rights reserved.
+//
+
+import XCTest
+@testable import BudgetMeApp
+
+class SavingsViewModelTests: XCTestCase, SavingsStubProtocol {
+
+    var savingsViewModel: SavingsViewModel!
+
+    override func setUp() {
+
+    }
+
+    override func tearDown() {
+        savingsViewModel = nil
+    }
+
+    func testRefreshData() {
+        savingsViewModel = SavingsViewModel(provider: makeMoyaSuccessStub(type: .browse))
+
+        savingsViewModel.refreshData()
+    }
+
+    func testCreateNewSaving() {
+
+        savingsViewModel = SavingsViewModel(provider: makeMoyaSuccessStub(type: .browse))
+
+        savingsViewModel.createNewSaving(name: "Some saving")
+    }
+
+}
