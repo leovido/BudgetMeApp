1aae2f9b7f166a1179cb7ed646d5ffd4e7c5c6b7
Christian Ray Leovido <christian.leovido@gmail.com>
2020-04-13 23:46:48 +0100

    Update LoginViewModel.swift

--- BudgetMeApp/ViewModel/LoginViewModel.swift
+++ BudgetMeApp/ViewModel/LoginViewModel.swift
@@ -27,18 +27,14 @@ struct LoginViewModel: AlternativeViewModelBlueprint {
     struct LoginViewModelInput {
         var emailTextFieldChanged: Observable<String>
         var passwordTextFieldChanged: Observable<String>
-        var isRememberMeSelected: Observable<Bool>
-        var loginButtonTapped: Signal<()>
     }
 
     struct LoginViewModelOutput {
         var isEmailTextFieldValid: Driver<Bool>
         var isValid: Driver<Bool>
-
-        var loginCredentials: Observable<STAccessToken>
     }
 
-    var provider: MoyaProvider<STAuthentication> = MoyaNetworkManagerFactory.makeManager()
+    let provider: MoyaProvider<STAuthentication> = MoyaNetworkManagerFactory.makeManager()
 
     func transform(input: Input) -> Output {
 
@@ -50,21 +46,17 @@ struct LoginViewModel: AlternativeViewModelBlueprint {
             .map({ $0.count > 6 })
             .asDriver(onErrorJustReturn: false)
 
-        let isValid = Observable.combineLatest(isEmailTextFieldValueValid.asObservable(),
-                                               isPasswordTextFieldValueValid.asObservable())
+        let isValid = Observable.combineLatest(
+            isEmailTextFieldValueValid.asObservable(),
+            isPasswordTextFieldValueValid.asObservable()
+        )
             .map({ $0 && $1 })
             .asDriver(onErrorJustReturn: false)
 
-        input.loginButtonTapped
-        let loginCredentials = provider.rx
-            .request(.authenticate(refreshToken: Session.shared.refreshToken,
-            .map(STAccessToken.self)
-            .asObservable()
-            .share()
-
-        return Output(isEmailTextFieldValid: isEmailTextFieldValueValid,
-                      isValid: isValid,
-                      loginCredentials: loginCredentials)
+        return Output(
+            isEmailTextFieldValid: isEmailTextFieldValueValid,
+            isValid: isValid
+        )
     }
 
 }
